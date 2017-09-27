2c239d9a3bcf2d5d756bebd3e543f59acfc18018
Ville <villekarjala@gmail.com>
2017-09-27 20:52:37 +0300

    admin skeleton functionality added

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
 
   class Cat {
     constructor(name, id) {
@@ -16,18 +16,19 @@ $(function(){
     }
   }
 
-let data = {
-  cats: undefined
-}
+  let data = {
+    cats: undefined,
+    admin: false
+  }
 
-let octopus = {
+  let octopus = {
 
-    createCats : function(catNames) {
+    createCats: function(catNames) {
       data.cats = new Map();
       for (let i = 0; i < catNames.length; i++) {
-          let id = i+1;
-          let cat = new Cat(catNames[i], id);
-          data.cats.set(""+id,cat);
+        let id = i + 1;
+        let cat = new Cat(catNames[i], id);
+        data.cats.set("" + id, cat);
       }
     },
 
@@ -35,96 +36,177 @@ let octopus = {
       return data.cats;
     },
 
-  getCat: function(catId) {
-    return data.cats.get(catId);
-  },
+    getCat: function(catId) {
+      return data.cats.get(catId);
+    },
 
     openCat: function(cat) {
-        let clickedCat = data.cats.get(""+cat.id);
-        catFocusView.render(clickedCat);
+      let clickedCat = data.cats.get("" + cat.id);
+      catFocusView.render(clickedCat);
+    },
+
+    isAdmin: function() {
+      return data.admin;
+    },
+
+    toggleAdminMode: function() {
+      data.admin = !data.admin;
+      catAdminView.render();
     },
 
     init: function() {
-        let catNames = ["Juma", "Dana", "Rontti", "Pekka", "Blakkis"];
-        this.createCats(catNames);
-        catListView.init();
-        catFocusView.init();
+      let catNames = ["Juma", "Dana", "Rontti", "Pekka", "Blakkis"];
+      this.createCats(catNames);
+      catListView.init();
+      catFocusView.init();
+      catAdminView.init();
     }
 
-};
+  };
 
-    let catListView = {
-        init: function() {
+  let catListView = {
+    init: function() {
 
-            // grab elements and html for using in the render function
-            this.$catList = $('.cat-list');
-            this.catListTemplate = $('script[data-template="catlist"]').html();
+      // grab elements and html for using in the render function
+      this.$catList = $('.cat-list');
+      this.catListTemplate = $('script[data-template="catlist"]').html();
 
-            // Delegated event to listen for removal clicks
-            this.$catList.on('click', '.catItem', function(e) {
-                let cat = $(this).data();
-                octopus.openCat(cat);
-                return false;
-            });
+      // Delegated event to listen for removal clicks
+      this.$catList.on('click', '.catItem', function(e) {
+        let cat = $(this).data();
+        octopus.openCat(cat);
+        return false;
+      });
 
-            this.render();
-        },
+      this.render();
+    },
 
-        render: function() {
-            //TODO:maybe not needed
-            //Cache vars for use in forEach() callback (performance)
-            let $catList = this.$catList,
-                catListTemplate = this.catListTemplate;
+    render: function() {
+      //TODO:maybe not needed
+      //Cache vars for use in forEach() callback (performance)
+      let $catList = this.$catList,
+        catListTemplate = this.catListTemplate;
 
-            // Clear and render
-            $catList.html('');
+      // Clear and render
+      $catList.html('');
 
-            let cats = octopus.getCats();
-            for (var key of cats.keys()) {
-              let cat = cats.get(key);
-              let thisTemplate = catListTemplate.replace(/{{id}}/g, cat.id);
-              thisTemplate = thisTemplate.replace(/{{name}}/g, cat.name);
-              $catList.append(thisTemplate);
-            }
-        }
-    };
+      let cats = octopus.getCats();
+      for (var key of cats.keys()) {
+        let cat = cats.get(key);
+        let thisTemplate = catListTemplate.replace(/{{id}}/g, cat.id);
+        thisTemplate = thisTemplate.replace(/{{name}}/g, cat.name);
+        $catList.append(thisTemplate);
+      }
+    }
+  };
 
 
-let catFocusView = {
+  let catFocusView = {
 
     init: function() {
 
-        // grab elements and html for using in the render function
-        this.$catFocus = $('#cat-focus');
-        this.catFocusTemplate = $('script[data-template="catfocus"]').html();
+      // grab elements and html for using in the render function
+      this.$catFocus = $('#cat-focus');
+      this.catFocusTemplate = $('script[data-template="catfocus"]').html();
 
-        // Delegated event to listen for removal clicks
-        this.$catFocus.on('click', '#catfocusimage', function(e) {
-            //TODO:it would be better to refa this info to model. Store currently selected id in model, not in html-view?
-            let catId = $(this).data();
-            let cat = octopus.getCat(""+catId.id);
-            //TODO: is this against the rule: view should not access model-
-            cat.increaseClick();
-            let clickCount = cat.getClickCount();
-            $("#clickCount").text(clickCount);
-        });
+      // Delegated event to listen for removal clicks
+      this.$catFocus.on('click', '#catfocusimage', function(e) {
+        //TODO:it would be better to refa this info to model. Store currently selected id in model, not in html-view?
+        let catId = $(this).data();
+        let cat = octopus.getCat("" + catId.id);
+        //TODO: is this against the rule: view should not access model-
+        cat.increaseClick();
+        let clickCount = cat.getClickCount();
+        $("#clickCount").text(clickCount);
+      });
     },
 
     render: function(cat) {
-       //TODO:maybe not needed
-        let $catFocus = this.$catFocus,
-            catFocusTemplate = this.catFocusTemplate;
+      //TODO:maybe not needed
+      let $catFocus = this.$catFocus,
+        catFocusTemplate = this.catFocusTemplate;
 
-        // Clear and render
-        $catFocus.html('');
+      // Clear and render
+      $catFocus.html('');
 
-        //TODO:replacing looks too complicated...
-        let thisTemplate = catFocusTemplate.replace(/{{id}}/g, cat.id);
-        thisTemplate = thisTemplate.replace(/{{name}}/g, cat.name);
-        thisTemplate = thisTemplate.replace(/{{clickCount}}/g, cat.clickCount);
-        //TODO:redraws whole html section every time. could this be done more precisely..
-        $catFocus.append(thisTemplate);
+      //TODO:replacing looks too complicated...
+      let thisTemplate = catFocusTemplate.replace(/{{id}}/g, cat.id);
+      thisTemplate = thisTemplate.replace(/{{name}}/g, cat.name);
+      thisTemplate = thisTemplate.replace(/{{clickCount}}/g, cat.clickCount);
+      //TODO:redraws whole html section every time. could this be done more precisely..
+      $catFocus.append(thisTemplate);
     }
-};
-    octopus.init();
-  }());
+  };
+
+
+  let catAdminView = {
+
+    init: function() {
+
+      // grab elements and html for using in the render function
+      this.$catAdmin = $('#admin');
+      //this.catFocusTemplate = $('script[data-template="catfocus"]').html();
+
+        if (octopus.isAdmin()) {
+            console.log("app is in admin mode");
+        }
+
+        else {
+          console.log("app is not in admin mode");
+        }
+
+
+      // Delegated event to listen for removal clicks
+      this.$catAdmin.on('click', function(e) {
+
+        /*
+        //TODO:it would be better to refa this info to model. Store currently selected id in model, not in html-view?
+        let catId = $(this).data();
+        let cat = octopus.getCat("" + catId.id);
+        //TODO: is this against the rule: view should not access model-
+        cat.increaseClick();
+        let clickCount = cat.getClickCount();
+        $("#clickCount").text(clickCount);
+        */
+        console.log("admin clicked");
+        octopus.toggleAdminMode();
+      });
+
+    },
+
+    render: function() {
+        if (octopus.isAdmin()) {
+          console.log("render admin mode");
+        }
+        else{
+          console.log("render not admin mode");
+        }
+      /*
+      //TODO:maybe not needed
+      let $catFocus = this.$catFocus,
+        catFocusTemplate = this.catFocusTemplate;
+
+      // Clear and render
+      $catFocus.html('');
+
+      //TODO:replacing looks too complicated...
+      let thisTemplate = catFocusTemplate.replace(/{{id}}/g, cat.id);
+      thisTemplate = thisTemplate.replace(/{{name}}/g, cat.name);
+      thisTemplate = thisTemplate.replace(/{{clickCount}}/g, cat.clickCount);
+      //TODO:redraws whole html section every time. could this be done more precisely..
+      $catFocus.append(thisTemplate);
+      */
+    }
+
+  };
+
+
+
+
+
+
+
+
+
+  octopus.init();
+}());
